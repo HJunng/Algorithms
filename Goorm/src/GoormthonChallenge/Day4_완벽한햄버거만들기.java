d4d40bbe3bf4838a990e5fcb60ccf80461ccab4e
guswjd1gh <guswjd1gh@gmail.com>
2023-08-17 14:41:55 +0900

    구름톤 챌린지 4일차 - 증가하는 수열 - [0817]

---
+++ Goorm/src/GoormthonChallenge/Day4_완벽한햄버거만들기.java
@@ -0,0 +1,34 @@
+package GoormthonChallenge;
+
+import java.io.*;
+import java.util.*;
+
+public class Day4_완벽한햄버거만들기 {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = Integer.parseInt(br.readLine());
+		
+		int[] ing = new int[cnt];
+		String[] s = br.readLine().split(" ");
+		for(int i=0;i<cnt;i++) ing[i]=Integer.parseInt(s[i]);
+		
+		int leftIdx = 0;
+		for(int i=0;i<cnt;i++){
+			if(ing[i]<ing[leftIdx]) break;
+			leftIdx=i;
+		}
+		
+		int rightIdx=cnt-1;
+		for(int i=cnt-1;i>=0;i--){
+			if(ing[i]<ing[rightIdx]) break;
+			rightIdx=i;
+		}
+		
+		if(leftIdx>=rightIdx) System.out.println(Arrays.stream(ing).sum());
+		else System.out.println(0);
+	}
+
+}
