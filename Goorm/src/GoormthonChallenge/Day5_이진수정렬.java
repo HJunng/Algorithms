49c461c296d3e3b4a9ba484dbee8a0b121e085a9
HJunng <guswjd1gh@gmail.com>
2023-08-22 16:10:57 +0900

    구름톤 챌린지 문제 upload

---
+++ Goorm/src/GoormthonChallenge/Day5_이진수정렬.java
@@ -0,0 +1,34 @@
+package GoormthonChallenge;
+
+import java.util.*;
+import java.io.*;
+public class Day5_이진수정렬 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] s = br.readLine().split(" ");
+		int n = Integer.parseInt(s[0]);
+		int k = Integer.parseInt(s[1]);
+		
+		int[][] num = new int[n][2];
+		s = br.readLine().split(" ");
+		for(int i=0;i<n;i++){
+			num[i][0]=Integer.parseInt(s[i]);
+			
+			int number = num[i][0];
+			while(number>1){
+				num[i][1]+=number%2;
+				number/=2;
+			}
+			num[i][1]+=1;
+		}
+		
+		Arrays.sort(num, new Comparator<int[]>(){
+			public int compare(int[] a, int[] b){
+				if(a[1]!=b[1]) return b[1]-a[1];
+				else return b[0]-a[0];
+			} 
+		});
+		
+		System.out.println(num[k-1][0]);
+	}
+}
---
+++ Goorm/src/GoormthonChallenge/Day6_문자열나누기.java
@@ -0,0 +1,35 @@
+package GoormthonChallenge;
+
+import java.io.*;
+import java.util.*;
+public class Day6_문자열나누기 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		
+		Set<String> splitSet = new HashSet<>();
+		ArrayList<String[]> order = new ArrayList<>();
+		
+		for(int i=1;i+1<s.length();i++){
+			for(int j=i+1;j<s.length();j++){
+				splitSet.add(s.substring(0,i));
+				splitSet.add(s.substring(i,j));
+				splitSet.add(s.substring(j,s.length()));
+				
+				order.add(new String[]{s.substring(0,i),s.substring(i,j),s.substring(j,s.length())});
+			}
+		}
+		
+		ArrayList<String> split = new ArrayList<>(splitSet);
+		Collections.sort(split);
+		
+		int score=0;
+		for(String[] o: order){
+			score=Math.max(score, split.indexOf(o[0])+split.indexOf(o[1])+split.indexOf(o[2])+3);
+		}
+		System.out.println(score);
+		
+	}
+}
