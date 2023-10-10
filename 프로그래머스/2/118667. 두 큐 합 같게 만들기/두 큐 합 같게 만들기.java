b0be4643e2a9a8b3360dc6282026a84a6379123d
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2023-10-10 12:42:37 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 62.06 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
+        
+        long sumQ1=0; long sumQ2 = 0; // 각 queue의 합
+        int size = queue1.length; //queue1, queue2의 길이
+        
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        
+        for(int i=0;i<size;i++){
+            q1.add(queue1[i]);
+            sumQ1+=queue1[i];
+            q2.add(queue2[i]);
+            sumQ2+=queue2[i];
+        }
+        
+        // 합이 홀수이면 둘로 나눌 수 없음 -> -1 리턴.
+        if((sumQ1+sumQ2)%2==1) return -1; 
+        
+        
+        int cnt=0; //움직인 횟수.
+        while(sumQ1!=sumQ2){
+            if(cnt>size*4) return -1;//q1과 q2가 두번씩 돌아가는 동안 찾지 못하면 못 찾는것.
+            
+            if(sumQ1<sumQ2){//q1의 원소합이 q2 원소합보다 작으면
+                sumQ1+=q2.peek();
+                sumQ2-=q2.peek();
+                q1.add(q2.poll());//q2에서 q1에 집어넣음
+                
+            }else{ //q1의 원소합이 half보다 크면
+                sumQ1-=q1.peek(); 
+                sumQ2+=q1.peek();
+                q2.add(q1.poll());//q1에서 원소를 poll해 q2에 넣는다.
+            }
+            cnt++;//작업 횟수 +1
+        }
+        
+        return cnt;
+    }
+}
