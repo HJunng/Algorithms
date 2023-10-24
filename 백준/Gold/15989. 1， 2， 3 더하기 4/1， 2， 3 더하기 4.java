73b83c82bd8d682c0e06c0b9e8281da52ca6197e
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2023-10-24 12:25:35 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 260 ms, Memory: 18364 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -7,44 +7,28 @@ public class Main {
 
 		Scanner s = new Scanner(System.in);
 		int T = s.nextInt();
+		
 		int[] ns = new int[T];
-		
-		int maxN=0;
+		int maxN=3;
 		for(int i=0;i<T;i++) {
-			ns[i] = s.nextInt();
-			if(ns[i]>maxN) maxN = ns[i];
+			ns[i]=s.nextInt();
+			maxN = Math.max(maxN, ns[i]);
 		}
 		
-		// 1: 1만 사용, 2: 2~1 사용, 3:3~1 사용 
-		int[][] dp = new int[maxN+1][4];
+		int[] dp = new int[maxN+1];
+//		dp[1]=1; // 1
+//		dp[2]=2; // 11 2
+//		dp[3]=3; // 111 21 3
 		
-		dp[1][1]=1; // 1
-		dp[1][2]=0;
-		dp[1][3]=0;
-		
-		if(maxN>=2) {
-			dp[2][1]=1; // 11
-			dp[2][2]=1; //2
-			dp[2][3]=0;
-		}
-		if(maxN>=3) {
-			dp[3][1]=1; // 111
-			dp[3][2]=1; // 21
-			dp[3][3]=1; // 3
+		for(int i=1;i<=3;i++) {
+			dp[i]+=1; // 자기자신으로 만들 수 있는 수 : 1, 2, 3 
+			
+			for(int j=i;j<=maxN;j++) {
+				dp[j] += dp[j-i]; // i를 사용하기 전의 경우의수 + i 사용!
+			}
 		}
 		
-		//dp[4][1] = 1111
-		//dp[4][2] = 112 22
-		//dp[4][3] = 13
+		for(int i=0;i<T;i++) System.out.println(dp[ns[i]]);
 		
-		for(int i=4;i<=maxN;i++) fillDP(i,dp);
-		
-		for(int i=0;i<T;i++) System.out.println(dp[ns[i]][1]+dp[ns[i]][2]+dp[ns[i]][3]);
-		
-	}
-	static void fillDP(int now, int[][] dp) {
-		dp[now][1] = dp[now-1][1];
-		dp[now][2] = dp[now-2][1]+dp[now-2][2];
-		dp[now][3] = dp[now-3][1]+dp[now-3][2]+dp[now-3][3];
 	}
 }
