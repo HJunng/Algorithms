cbbad86622a485f5db68b5cc0b683a7dcd2ee4ff
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2023-10-27 22:39:30 +0900

    [Silver III] Title: 1로 만들기, Time: 216 ms, Memory: 18588 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,16 +1,41 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main {
+
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int dp[] = new int [n+1];
-		dp[0]=dp[1]=0;
-		for(int i=2;i<=n;i++) {
-			dp[i]=dp[i-1]+1;
-			if(i%2==0) dp[i] = Math.min(dp[i], dp[i/2]+1);
-			if(i%3==0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+		int x = s.nextInt();
+		
+		System.out.println(bfs(x));
+	}
+	private static int bfs(int x) {
+		
+		boolean[] visited = new boolean[x+1];
+		visited[x]=true;
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {x,0});
+		
+		int minCnt=0;
+		while(!q.isEmpty()) {
+			 int[] now = q.poll();
+			 
+			 if(now[0]==1) {
+				 minCnt=now[1]; break;
+			 }
+			 
+			 if(now[0]%3==0 && !visited[now[0]/3]) {
+				 visited[now[0]/3]=true;
+				 q.add(new int[] {now[0]/3,now[1]+1});
+			 }
+			 if(now[0]%2==0 && !visited[now[0]/2]) {
+				 visited[now[0]/2]=true;
+				 q.add(new int[] {now[0]/2,now[1]+1});
+			 }
+			 if(!visited[now[0]-1]) {
+				 visited[now[0]-1]=true;
+				 q.add(new int[] {now[0]-1,now[1]+1});
+			 }
 		}
-		System.out.println(dp[n]);
+		return minCnt;
 	}
 }
