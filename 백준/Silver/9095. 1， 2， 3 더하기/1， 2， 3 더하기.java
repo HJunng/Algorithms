2fa3c94e6a1c9d711222e7790f0ad2bf30a0a338
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2023-10-27 22:49:51 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,28 +1,30 @@
 import java.util.*;
+
 public class Main {
-	static int answer;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Scanner s = new Scanner(System.in);
-		int tc = s.nextInt();
-		for(int i=0;i<tc;i++) {
-			answer=0;
-			dfs(s.nextInt());
-			System.out.println(answer);
+		int T = s.nextInt();
+		
+		int[] ns = new int[T];
+		int max=0;
+		for(int i=0;i<T;i++) {
+			ns[i] = s.nextInt();
+			max = Math.max(ns[i], max);
 		}
 		
-	}
-	//깊이 우선 탐색
-	private static void dfs(int n) {
-		// TODO Auto-generated method stub
+		int[] dp = new int[max+1];
+		dp[1] = 1; // 1
+		dp[2] = 2; // 1+1, 2
+		dp[3] = 4; // 1+1, 1+2, 2+1, 3
+		dp[4] = 7; // 1+1+1+1, 2+1+1, 1+2+1, 1+1+2, 2+2, 3+1, 1+3
 		
-		if(n==0) {
-			answer++;return;
+		for(int i=5;i<=max;i++) {
+			dp[i] = dp[i-3]+dp[i-2]+dp[i-1];
 		}
-		if(n-3>=0) dfs(n-3);
-		if(n-2>=0) dfs(n-2);
-		if(n-1>=0) dfs(n-1);
+		
+		for(int i=0;i<T;i++) System.out.println(dp[ns[i]]);
 	}
 }
