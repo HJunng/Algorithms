a13d29ee9cf61e61a44e8f7fd745dff965fb2297
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2023-10-28 15:27:44 +0900

    [Silver III] Title: 2×n 타일링, Time: 220 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+	static int mod=10007;
+	static int[] sum;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		sum = new int[n+1];
+		
+		dp(n);
+		
+		System.out.println(sum[n]%mod);
+		
+	}
+	static private void dp(int n) {
+		
+		if(n>=1) sum[1]=1;
+		if(n>=2) sum[2]=2;
+		
+		for(int i=3;i<=n;i++)
+			sum[i]=(sum[i-1]%mod+sum[i-2]%mod)%mod;
+	}
+}
