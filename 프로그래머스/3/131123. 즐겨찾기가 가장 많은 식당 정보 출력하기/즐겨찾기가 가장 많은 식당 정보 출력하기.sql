60b4be95bf0a0ccabc593ea53bd98548848188df
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-01-23 11:39:17 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,13 +1,27 @@
--- food_type을 기준으로 가장 큰 즐겨찾기 수를 가진 튜플 뽑아내기
-# select FOOD_TYPE, REST_NAME, max(FAVORITES)
-# from REST_INFO
-# group by food_type
+# select a.FOOD_TYPE, a.REST_ID, a.REST_NAME, a.FAVORITES
+# from REST_INFO as a join (
+#     select FOOD_TYPE, max(FAVORITES) as maxFav
+#     from REST_INFO
+#     group by FOOD_TYPE
+# ) as b
+# on a.FOOD_TYPE = b.FOOD_TYPE and a.FAVORITES = b.maxFav
+# order by a.FOOD_TYPE desc
 
-select a.FOOD_TYPE, a.REST_ID, a.REST_NAME, a.FAVORITES
-from REST_INFO as a inner join (
-    select FOOD_TYPE, max(FAVORITES) as maxFav
-    from REST_INFO
-    group by FOOD_TYPE
-) as b
-on a.FOOD_TYPE = b.FOOD_TYPE and a.FAVORITES = b.maxFav
-order by a.FOOD_TYPE desc
+SELECT
+  FOOD_TYPE,
+  REST_ID AS ID,
+  REST_NAME AS 식당이름,
+  FAVORITES AS 즐겨찾기수
+FROM
+  REST_INFO r1
+WHERE
+  FAVORITES = (
+    SELECT
+      MAX(FAVORITES)
+    FROM
+      REST_INFO r2
+    WHERE
+      r1.FOOD_TYPE = r2.FOOD_TYPE
+  )
+ORDER BY
+  FOOD_TYPE DESC;
