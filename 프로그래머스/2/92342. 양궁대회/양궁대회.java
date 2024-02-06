1804f2d2e3f59e35bb26c34fe6f74b67f7942d8f
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-02-06 22:37:53 +0900

    [level 2] Title: 양궁대회, Time: 2.13 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class Solution {
+    static ArrayList<Integer> scoreList = new ArrayList<>();
+    static int maxSubt = 0;
+    public int[] solution(int n, int[] info) {
+        int[] answer = {};
+        
+        dfs(0,0,0,new ArrayList<Integer>(),n,info);
+        
+        if(scoreList.isEmpty()) return new int[]{-1};
+        
+        answer = new int[11];
+        for(int i=0;i<11;i++) answer[i]=scoreList.get(i);
+        
+        return answer;
+    }
+    void dfs(int idx, int apeach, int lion, ArrayList<Integer> score, int n, int[] info){
+        if(idx==info.length){ //끝까지 탐색했을 때
+            if(apeach<lion && lion-apeach>maxSubt){ //가장 큰 점수차보다 크면
+                scoreList = new ArrayList<>(score);
+                maxSubt=lion-apeach;
+            }else if(apeach<lion && lion-apeach==maxSubt){//가장 큰 점수차의 경우가 여러 개일때
+                
+                for(int i=idx-1;i>=0;i--){//가장 작은 점수를 더 많이 맞힌 경우 return
+                    if(score.get(i)>scoreList.get(i)){
+                        scoreList = new ArrayList<>(score);
+                        break;
+                    }else if(score.get(i)<scoreList.get(i)) break;
+                }
+            }
+            return;
+        }
+        
+        if(idx==info.length-1){
+            score.add(n);
+            dfs(idx+1,apeach,lion,score,0,info);
+            score.remove(score.size()-1);
+            return;
+        }
+        
+        if(n>info[idx]){
+            score.add(info[idx]+1);
+            dfs(idx+1,apeach,lion+(10-idx),score,n-info[idx]-1,info);
+            score.remove(score.size()-1);
+        }
+
+        score.add(0);
+        if(info[idx]==0) dfs(idx+1,apeach,lion,score,n,info);
+        else dfs(idx+1,apeach+(10-idx),lion,score,n,info);
+        score.remove(score.size()-1);
+        
+        
+    }
+}
