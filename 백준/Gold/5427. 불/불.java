b8c0d12dbf3949b15db57d4bc602cd666b3efa40
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-04-03 00:35:39 +0900

    [Gold IV] Title: 불, Time: 660 ms, Memory: 123636 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -2,95 +2,122 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int[] dx = {1, -1, 0, 0};
-    static final int[] dy = {0, 0, 1, -1};
-
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    static int w,h;
     static char[][] map;
-    static int w, h;
 
+    static int time;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
 
-        while (tc-- > 0) {
-            String[] wh = br.readLine().split(" ");
-            w = Integer.parseInt(wh[0]);
-            h = Integer.parseInt(wh[1]);
+        while(tc-->0){
+            String[] s = br.readLine().split(" ");
+
+            w = Integer.parseInt(s[0]);
+            h = Integer.parseInt(s[1]);
+
             map = new char[h][w];
+            int[] start = new int[2];
 
-            Point start = null;
-
-            for (int i = 0; i < h; i++) {
+            for(int i=0;i<h;i++){ // map 저장
                 String line = br.readLine();
-                for (int j = 0; j < w; j++) {
+                for(int j=0;j<w;j++){
                     map[i][j] = line.charAt(j);
-                    if (map[i][j] == '@') start = new Point(i, j);
+                    if(map[i][j]=='@'){ // 시작 위치 저장
+                        start[0] = i;
+                        start[1] = j;
+                    }
                 }
             }
 
-            int result = escape(start);
-            if (result == -1) System.out.println("IMPOSSIBLE");
+            time = 0; // 현재 시간 (불이 얼마나 번졌는지를 확인하기 위해 사용)
+            int result = bfs(start);
+
+            if(result == -1) System.out.println("IMPOSSIBLE");
             else System.out.println(result);
         }
     }
+    private static int bfs(int[] start){
 
-    static int escape(Point start) {
-        Queue<Point> q = new LinkedList<>();
-        Queue<Point> fire = new LinkedList<>();
-        boolean[][] visited = new boolean[h][w];
+        Queue<int[]> q = new LinkedList<>();
+        Queue<int[]> fire = new LinkedList<>();
 
-        q.offer(start);
-        visited[start.x][start.y] = true;
+        q.add(new int[]{start[0],start[1],0}); // 현재 위치 행, 열, 걸린시간
+        map[start[0]][start[1]] = '!'; //방문 처리
 
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                if (map[i][j] == '*') fire.offer(new Point(i, j));
+        for(int i=0;i<h;i++){
+            for(int j=0;j<w;j++){
+                if(map[i][j]=='*') fire.add(new int[]{i,j}); // 불의 행, 열
             }
         }
 
-        int time = 0;
+        while(!q.isEmpty()){
 
-        while (!q.isEmpty()) {
             // 불이 번짐
             int size = fire.size();
-            for (int i = 0; i < size; i++) {
-                Point cur = fire.poll();
-                for (int j = 0; j < 4; j++) {
-                    int nx = cur.x + dx[j];
-                    int ny = cur.y + dy[j];
-                    if (nx >= 0 && nx < h && ny >= 0 && ny < w && map[nx][ny] != '#' && map[nx][ny] != '*') {
-                        map[nx][ny] = '*';
-                        fire.offer(new Point(nx, ny));
+            for(int i=0;i<size;i++){
+                int[] f = fire.poll();
+                for(int dir=0;dir<4;dir++){
+                    int nx = f[0] + dx[dir];
+                    int ny = f[1] + dy[dir];
+                    // 범위를 벗어나지 않고, 벽이 아니고, 이미 불이 붙은자리가 아닐 때
+                    if(nx>=0 && nx<h && ny>=0 && ny<w && map[nx][ny]!='#' && map[nx][ny]!='*'){
+                        map[nx][ny]='*';
+                        fire.add(new int[]{nx,ny});
                     }
                 }
             }
 
             // 상근이 이동
             size = q.size();
-            for (int i = 0; i < size; i++) {
-                Point cur = q.poll();
-                if (cur.x == 0 || cur.x == h - 1 || cur.y == 0 || cur.y == w - 1) return time + 1;
-                for (int j = 0; j < 4; j++) {
-                    int nx = cur.x + dx[j];
-                    int ny = cur.y + dy[j];
-                    if (nx >= 0 && nx < h && ny >= 0 && ny < w && !visited[nx][ny] && map[nx][ny] == '.') {
-                        visited[nx][ny] = true;
-                        q.offer(new Point(nx, ny));
+            for(int i=0;i<size;i++){
+                int[] now = q.poll();
+
+                // map의 벽에 다다르면 (현재까지의 시간 +1) 해서 나가는게 가장 빠른길
+                if(now[0]==0 || now[0]==h-1 || now[1]==0 || now[1]==w-1)
+                    return now[2]+1;
+
+                // 상하좌우 이동
+                for(int dir=0;dir<4;dir++){
+                    int nx = now[0]+dx[dir];
+                    int ny = now[1]+dy[dir];
+                    if(nx>=0 && nx<h && ny>=0 && ny<w && map[nx][ny]=='.'){
+                        map[nx][ny]='!';
+                        q.add(new int[]{nx,ny,now[2]+1});
                     }
                 }
             }
-            time++;
         }
 
+        // 탈출 못함.
         return -1;
     }
+    private static void fireMove(){
 
-    static class Point {
-        int x, y;
+        // 불이 있는 위치 따로 저장
+        ArrayList<int[]> fireLoc = new ArrayList<>();
 
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        for(int i=0;i<h;i++){
+            for(int j=0;j<w;j++){
+                if(map[i][j]=='*'){
+                    fireLoc.add(new int[]{i,j});
+                }
+            }
+        }
+
+        // 불 번짐 (상하좌우)
+        for(int[] loc : fireLoc){
+            for(int i=0;i<4;i++){
+                int nx = loc[0]+dx[i];
+                int ny = loc[1]+dy[i];
+
+                if(nx<0 && nx>=h || ny<0 || ny>=w) continue;
+
+                if(map[nx][ny]=='.' || map[nx][ny]=='!') // 범위를 벗어나지 않고, 벽 아님
+                    map[nx][ny] = '*';
+            }
         }
     }
 }
