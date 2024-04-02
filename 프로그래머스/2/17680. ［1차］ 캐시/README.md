adcc2fbd2242c95c2008e0dad62280f4ed36fbc2
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-04-02 10:56:56 +0900

    [level 2] Title: [1차] 캐시, Time: 3.05 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 1.86 ms
+메모리: 79.7 MB, 시간: 3.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 1:37:12
+2024년 04월 02일 10:56:54
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,34 +1,38 @@
 import java.util.*;
+
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
         
-        //캐시사이즈가 0일때는 반복문 필요없이 (도시개수*5)하면 됨.
-        if(cacheSize==0) return cities.length*5;
-        
-        Queue<String> q = new LinkedList<>();
-        
+        LinkedList<String> cache = new LinkedList<>();
         for(int i=0;i<cities.length;i++){
-            //대소문자 구분 없애기 위해 모두 소문자 사용.
             String city = cities[i].toLowerCase();
             
-            //게시물 가져오는데 걸린 시간 추가
-            if(q.contains(city)){
-                //cache-hit
-                answer+=1; 
-                
-                //최근 사용했던 순으로 넣어야 하기 때문에 기존 자리에서 지워줌.
-                q.remove(city);
+            // 캐시가 city를 가지고 있는 우
+            if(cache.contains(city)){
+                answer += 1;
+                for(String c : cache){
+                    if(c.equals(city)){
+                        cache.remove(city);
+                        break;
+                    }
+                }
+                cache.add(city);
             }
-            else
-                answer+=5; //cache-miss
-            
-            //캐시 삭제 -> 최근 사용했던 순으로 도시 넣기
-            if(q.size()>=cacheSize)
-                q.poll();
-            //캐시 추가
-            q.add(city);
+            // 캐시가 city를 가지고 있지 않은데, 캐시가 다 차지 않은 경우
+            else if(cache.size()<cacheSize){
+                cache.add(city);
+                answer += 5;
+            }
+            else if(cache.size()==0) answer += 5;
+            // 캐시가 city를 가지고 있지 않고, 캐시가 다 찬 경우
+            else{
+                answer += 5;
+                cache.poll();
+                cache.add(city);
+            }
         }
+        
         return answer;
     }
 }
