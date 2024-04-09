e0416a755b673b8c10f12b4e2332ec638eacc54f
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-04-09 10:41:41 +0900

    [level 2] Title: 튜플, Time: 0.47 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -4,24 +4,39 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = {};
         
-        s = s.replace("{","");
-        s = s.replace("}","");
+        s = s.substring(2,s.length()-2);
         
-        String[] nums = s.split(",");
-        
-        Map<Integer,Integer> numCnt = new HashMap<>();
-        for(String num : nums){
-            int n = Integer.parseInt(num);
-            numCnt.put(n, numCnt.getOrDefault(n,0)+1);
+        String[] temp = s.split("\\},\\{"); // },{
+        String[][] tuple = new String[temp.length][];
+        for(int i=0;i<temp.length;i++){
+            tuple[i] = temp[i].split(",");
         }
         
-        ArrayList<Integer> key = new ArrayList<>(numCnt.keySet());
-        Collections.sort(key, (a,b)-> numCnt.get(b)-numCnt.get(a));
+        Arrays.sort(tuple, new Comparator<String[]>(){
+            public int compare(String[] a, String[] b){
+                return Integer.compare(a.length,b.length);
+            }
+        });
         
-        answer = new int[key.size()];
-        for(int i=0;i<key.size();i++){
-            answer[i] = key.get(i);
+        // Comparator<String> comparator = Comparator.comparingInt(String::length);
+        // Arrays.sort(array, comparator);
+        
+        answer = new int[tuple.length];
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0;i<tuple.length;i++){
+            for(int j=0;j<tuple[i].length;j++){
+                int num = Integer.parseInt(tuple[i][j]);
+                if(!set.contains(num)){
+                    answer[i] = num;
+                    set.add(num);
+                }
+            }
         }
+        
+        
+        // System.out.println(Arrays.toString(tuple));
+        // System.out.println(tuple[0]);
         
         return answer;
     }
