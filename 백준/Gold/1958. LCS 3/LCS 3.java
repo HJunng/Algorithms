6748fade1ffa54348b44cbbcc0b5f698510254c8
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-04-17 02:03:12 +0900

    [Gold IV] Title: LCS 3, Time: 188 ms, Memory: 20644 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.java
+++ 백준/Gold/1958. LCS 3/LCS 3.java
@@ -1,20 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] str = new String[3];
-
         for(int i=0;i<3;i++) str[i] = br.readLine();
-
 
         int[][][] lcs = new int[str[0].length()+1][str[1].length()+1][str[2].length()+1];
 
-        int max=0;
         for(int i=1;i<=str[0].length();i++){
             for(int j=1;j<=str[1].length();j++){
                 for(int k=1;k<=str[2].length();k++){
@@ -25,12 +20,10 @@ public class Main {
                         // lcs[i-1][j][k], lcs[i][j-1][k], lcs[i][j][k-1] 중에 가장 큰 수
                         lcs[i][j][k] = Math.max(Math.max(lcs[i-1][j][k],lcs[i][j-1][k]), lcs[i][j][k-1]);
                     }
-
-                    max = Math.max(max, lcs[i][j][k]);
                 }
             }
         }
 
-        System.out.println(max);
+        System.out.println(lcs[str[0].length()][str[1].length()][str[2].length()]);
     }
 }
