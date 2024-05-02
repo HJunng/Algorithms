6a229349e7806420f2ec4c52c4b693f27df13ec3
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-05-02 10:11:57 +0900

    [level 3] Title: 야근 지수, Time: 113.31 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,19 +3,21 @@ class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
         
-        // 작업량이 많은 순으로 정렬되게 일을 pq에 넣음.
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        
         for(int i=0;i<works.length;i++){
             pq.add(works[i]);
         }
         
-        for(int i=0;i<n;i++){
-            if(pq.peek()==0) break;
-            pq.add(pq.poll()-1);
+        while(n-->0 && !pq.isEmpty()){
+             int now = pq.poll();
+            if(now>0) pq.add(now-1); 
         }
         
-        for(int i=0;i<works.length;i++)
-            answer+=Math.pow(pq.poll(),2);
+        for(int remain : pq){
+            answer += Math.pow(remain,2);
+        }
+        
         
         return answer;
     }
