1acff24a7134d8abbec90510da1f4fa7c138bd3e
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-05-23 10:17:18 +0900

    [level 2] Title: 스킬트리, Time: 12.39 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        for(int i=0;i<skill_trees.length;i++){
+            String s = skill_trees[i];
+            String removeOthers = "";
+            
+            for(int j=0;j<s.length();j++){
+                for(int k=0;k<skill.length();k++){
+                    if(s.charAt(j)==skill.charAt(k)){
+                        removeOthers+=s.charAt(j);
+                    }
+                }
+            }
+            
+            if(removeOthers.equals(skill.substring(0,removeOthers.length()))) answer++;
+        }
+        
+        
+        return answer;
+    }
+}
