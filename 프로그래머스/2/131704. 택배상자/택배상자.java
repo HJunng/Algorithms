9cb7f2df5273493e27eeb85fbbb81abeafc9c787
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-05-30 10:18:58 +0900

    [level 2] Title: 택배상자, Time: 60.49 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        int idx=0;
+        
+        for(int i=1;i<=order.length;i++){
+            // 순서랑 맞으면 트럭에 싣고, 아니면 stack에 담아두기.
+            if(i==order[idx]){
+                idx++;
+            } else {
+                stack.push(i);
+            }
+            
+            while(!stack.isEmpty() && stack.peek()==order[idx]){
+                stack.pop();
+                idx++;
+            }
+        }
+        
+        return idx;
+    }
+}
