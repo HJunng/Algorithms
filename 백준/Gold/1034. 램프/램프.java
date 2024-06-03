0939c16aa6a021cfd89c0cf87b47d05af9ebdeed
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-06-04 01:26:52 +0900

    [Gold IV] Title: 램프, Time: 228 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Gold/1034. 램프/램프.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        String[] lamp = new String[n];
+        Map<String,Integer> pattern = new HashMap<>(); // 행 패턴 개수
+        for(int i=0;i<n;i++){
+            lamp[i] = br.readLine();
+            pattern.put(lamp[i], pattern.getOrDefault(lamp[i],0)+1);
+        }
+
+        int k = Integer.parseInt(br.readLine());
+        int isOdd = k%2;
+        if(k>50) k=50; // 최대 50열만 나올 수 있으므로 50까지 줄이기
+
+        int max = 0;
+        for(String p : pattern.keySet()){
+
+            int zeroCnt = 0; // 0 개수 파악
+            for(int i=0;i<p.length();i++){
+                if(p.charAt(i)=='0') zeroCnt++;
+            }
+
+            // 짝홀 맞아야함. -> 원하는 숫자 만들 수 있음.
+            if(zeroCnt%2 != isOdd) continue;
+
+            // 0 개수가 k개 이하여야 함.
+            if(zeroCnt<=k && pattern.get(p)>max){
+                max = pattern.get(p);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
