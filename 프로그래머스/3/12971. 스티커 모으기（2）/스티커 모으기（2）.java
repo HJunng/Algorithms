d4e7e079e8ca32ebc6669448c7d7a31ab44d8acc
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-06-29 22:52:06 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.65 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -13,6 +13,7 @@ class Solution {
         for(int i=2;i<len-1;i++){
             dpFirst[i] = Math.max(dpFirst[i-2]+sticker[i], dpFirst[i-1]);
             dpLast[i] = Math.max(dpLast[i-2]+sticker[i], dpLast[i-1]);
+            
         }
         
         if(len>=3) dpLast[len-1] = Math.max(dpLast[len-3]+sticker[len-1], dpLast[len-2]);
