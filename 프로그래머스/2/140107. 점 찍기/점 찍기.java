1eb688fbc81020843564ed3b0e568a0252665ba5
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-07-28 23:47:08 +0900

    [level 2] Title: 점 찍기, Time: 22.84 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        
+        int maxCoor=0;// a,b가 나올 수 있는 최대 좌표
+        for(int i=d;i>=1;i--){
+            if(i%k==0){
+                maxCoor=i;break;
+            }
+        }
+        
+        
+        int b=maxCoor;
+        for(long a=0;a<=maxCoor;a+=k){
+            // Math.sqrt((a*k)^2 +(b*k)^2* <= d가 될 때까지 b를 찾음
+            //제곱근 처리해주지 않으면 long형 범위를 벗어날 수 있어서 조심.
+            while(Math.sqrt(Math.pow(a,2)+Math.pow(b,2)) > d){
+                b-=k;
+                //System.out.println(b);
+            }
+            answer+=b/k+1;// +1은 (b=0일 때까지 셈.)
+            //System.out.println(a+" "+b+" : "+answer);
+        }
+        
+        return answer;
+    }
+}
