136d5b4e68e26fda51680cdaa7700b944d48436e
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-09-23 11:27:37 +0900

    [Gold I] Title: 구간 합 구하기, Time: 3792 ms, Memory: 99072 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,52 +1,54 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static long[] num, sum;
-    public static int n,m,k;
-    public static void main(String[] args) throws IOException {
+    static int N, M, K;
+    static long[] num, sum;
+    public static void main(String[] args) throws Exception{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); // 수의 개수
-        m = Integer.parseInt(st.nextToken()); // 변경 횟수
-        k = Integer.parseInt(st.nextToken()); // 구간합 구하는 횟수
 
-        num = new long[n];
-        sum = new long[n+1];
-        sum[0] = 0;
-        for(int i=0; i<n; i++){
+
+        N = Integer.parseInt(st.nextToken()); // 수의 개수
+        M = Integer.parseInt(st.nextToken()); // 변경 횟수
+        K = Integer.parseInt(st.nextToken()); // 구간 합 구하는 횟수
+
+        // N개의 수 입력받기
+        num = new long[N];
+        sum = new long[N];
+        for(int i=0;i<N;i++){
             num[i] = Long.parseLong(br.readLine());
-            sum[i + 1] = sum[i] + num[i];
+            sum[i] = (i==0)? num[i]:sum[i-1]+num[i];
         }
 
-        for(int i=0; i<m + k ; i++) {
+        // 수 변경 or 구간합
+        for(int i=0;i<M+K;i++){ // O(20000)
             st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            switch(a){
-                case 1:
-                    long c = Long.parseLong(st.nextToken());
-                    // b번째 수를 c로 바꾸기
-                    changeNum(b - 1, c);
-                    break;
-                case 2:
-                    // b번째 수 ~ c번째수의 합을 구하기
-                    int idx = Integer.parseInt(st.nextToken());
-                    System.out.println(sum[idx] - sum[b-1]);
-                    break;
+            long c = Long.parseLong(st.nextToken());
+
+            // 수 변경 : b-1번째 수를 c로 바꾸기
+            if(a==1){
+                change(b-1,c);
+            }
+            // 구간 합 구하기 : b-1번째 수부터 c-1번째 수까지 구간 합
+            // 1. 구간 합 매번 구하기 O(1,000,000)  -> 7% 시간초과
+            // 2. k번째 수까지의 합 배열 구해놓고, 수 변경될 때 갱신하기. -> 7% 시간초과
+            else {
+                if(b-1==0) System.out.println(sum[(int)c-1]);
+                else System.out.println(sum[(int)c-1]-sum[b-2]);
             }
         }
     }
-
-    // b 번째 수를 c의 값으로 변경하는 메서드
-    private static void changeNum(int b, long c) {
-        long originNum = num[b];
+    static void change(int b, long c){
+        long temp = num[b];
         num[b] = c;
-        for(int i = b + 1; i <= n; i++){
-            sum[i] -= originNum;
-            sum[i] += c;
+        for(int j=b;j<N;j++){
+            sum[j] = sum[j]-temp+c;
         }
     }
 }
