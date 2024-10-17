816eb2fb11a0450829279c7863e046ddd6e3ee23
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2024-10-17 11:06:50 +0900

    [Gold III] Title: 스터디 시간 정하기 1, Time: 464 ms, Memory: 84912 KB -BaekjoonHub

--- 백준/Gold/23295. 스터디 시간 정하기 1/스터디 시간 정하기 1.java
+++ 백준/Gold/23295. 스터디 시간 정하기 1/스터디 시간 정하기 1.java
@@ -1,53 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int[] possTime;
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.valueOf(st.nextToken());
+        int t=Integer.valueOf(st.nextToken());
 
-        int N = Integer.parseInt(s[0]); // 참가자 수
-        int T = Integer.parseInt(s[1]); // 스터디 시간
+        //지점마다 추가되고, 끝나는 사람의 수를 체크
+        int[] start=new int[100001];
+        int[] end=new int[100001];
+        for(int i=1; i<=n; i++) {
+            int num = Integer.valueOf(br.readLine());
+            for (int j = 1; j <= num; j++) {
+                st = new StringTokenizer(br.readLine());
+                int s = Integer.valueOf(st.nextToken());
+                int e = Integer.valueOf(st.nextToken());
 
-        possTime = new int[100_001];
-        int maxTime=0;
-        for(int i=0;i<N;i++){
-            int K = Integer.parseInt(br.readLine());
-            for(int j=0;j<K;j++){
-                s = br.readLine().split(" ");
+                start[s]++;
+                end[e]++;
+            }
+        }
+        //누적합도 진행하면서, 각 구간별로 수가 되고, 줄어든 사람 수를 변화해감.
+        long temp=0;
+        long[] people=new long[100001];
+        for(int i=0; i<=100000; i++){
+            people[i]+=temp;
+            if(i!=0){
+                people[i]+=people[i-1];
+            }
+            temp+=start[i]; //추가되는 사람의 수
+            temp-=end[i]; //감소되는 사람의 수
+        }
 
-                int start = Integer.parseInt(s[0]);
-                int end = Integer.parseInt(s[1]);
-                maxTime = Math.max(maxTime,end);
-
-                for(int k=start;k<end;k++){
-                    possTime[k]++;
-                }
+        //구간별로 구간 중에서 가장 큰 값을 뽑아내기
+        int room=t;
+        long max=people[t];
+        for(int i=t+1; i<=100000; i++){
+            long time=people[i]-people[i-t];
+            if(time>max){
+                max=time;
+                room=i;
             }
         }
 
-        // 처음 0~T초까지의 시간만족도
-        int maxSatis = 0;
-        int maxStart=0;
-        int maxEnd = T;
-        for(int i=0;i<T;i++){
-            maxSatis += possTime[i];
-        }
-
-        int nowStatis = maxSatis;
-        for(int i=T;i<=maxTime;i++){
-            nowStatis = nowStatis + possTime[i] - possTime[i-T];
-
-            if(nowStatis>maxSatis){
-                maxSatis = nowStatis;
-                maxEnd = i+1;
-                maxStart = i-T+1;
-            }
-        }
-
-        System.out.println(maxStart+" "+maxEnd);
+        bw.write((room-t)+" "+room);
+        bw.flush();
     }
 }
