06cecbb9b1992bc850a835576497215b8b39b1fc
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2025-02-06 19:26:07 +0900

    [Silver IV] Title: 덱, Time: 172 ms, Memory: 19004 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -14,51 +14,32 @@ public class Main {
 			str = br.readLine().split(" ");
 			
 			switch(str[0]) {
-			case("push_front"):{
-				d.addFirst(Integer.parseInt(str[1]));
-				break;
-			}
-			case("push_back"):{
-				d.addLast(Integer.parseInt(str[1]));
-				break;
-			}
+			case("push_front"): d.addFirst(Integer.parseInt(str[1])); break;
+			case("push_back"): d.addLast(Integer.parseInt(str[1])); break;
 			case("pop_front"):{
-				if(d.isEmpty())
-					sb.append(-1).append("\n");
-				else
-					sb.append(d.pollFirst()).append("\n");
+				if(d.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(d.pollFirst()).append("\n");
 				break;
 			}
 			case("pop_back"):{
-				if(d.isEmpty())
-					sb.append(-1).append("\n");
-				else
-					sb.append(d.pollLast()).append("\n");
+				if(d.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(d.pollLast()).append("\n");
 				break;
 			}
-			case("size"):{
-				sb.append(d.size()).append("\n");
-				break;
-			}
+			case("size"): sb.append(d.size()).append("\n"); break;
 			case("empty"):{
-				if(d.isEmpty())
-					sb.append("1\n");
-				else
-					sb.append("0\n");
+				if(d.isEmpty()) sb.append("1\n");
+				else sb.append("0\n");
 				break;
 			}
 			case("front"):{
-				if(d.isEmpty())
-					sb.append(-1).append("\n");
-				else
-					sb.append(d.peekFirst()).append("\n");
+				if(d.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(d.peekFirst()).append("\n");
 				break;
 			}
 			case("back"):{
-				if(d.isEmpty())
-					sb.append(-1).append("\n");
-				else
-					sb.append(d.peekLast()).append("\n");
+				if(d.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(d.peekLast()).append("\n");
 				break;
 			}
 			}
