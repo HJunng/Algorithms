6b039a1e81474f97fc607addfddb0a18b9ae0cc9
ImHyunjung <56528404+HJunng@users.noreply.github.com>
2025-02-06 19:31:04 +0900

    [Silver IV] Title: 덱, Time: 188 ms, Memory: 19120 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,49 +1,28 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb= new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		Deque<Integer> d = new ArrayDeque<>();
-		String str[];
-		
-		for(int i=0;i<n;i++) {
-			str = br.readLine().split(" ");
-			
-			switch(str[0]) {
-			case("push_front"): d.addFirst(Integer.parseInt(str[1])); break;
-			case("push_back"): d.addLast(Integer.parseInt(str[1])); break;
-			case("pop_front"):{
-				if(d.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(d.pollFirst()).append("\n");
-				break;
-			}
-			case("pop_back"):{
-				if(d.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(d.pollLast()).append("\n");
-				break;
-			}
-			case("size"): sb.append(d.size()).append("\n"); break;
-			case("empty"):{
-				if(d.isEmpty()) sb.append("1\n");
-				else sb.append("0\n");
-				break;
-			}
-			case("front"):{
-				if(d.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(d.peekFirst()).append("\n");
-				break;
-			}
-			case("back"):{
-				if(d.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(d.peekLast()).append("\n");
-				break;
-			}
-			}
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> d = new ArrayDeque<>();
+        String[] str;
+
+        for (int i = 0; i < n; i++) {
+            str = br.readLine().split(" ");
+            switch (str[0]) {
+                case "push_front" -> d.addFirst(Integer.parseInt(str[1]));
+                case "push_back" -> d.addLast(Integer.parseInt(str[1]));
+                case "pop_front" -> sb.append(d.isEmpty() ? -1 : d.pollFirst()).append("\n");
+                case "pop_back" -> sb.append(d.isEmpty() ? -1 : d.pollLast()).append("\n");
+                case "size" -> sb.append(d.size()).append("\n");
+                case "empty" -> sb.append(d.isEmpty() ? 1 : 0).append("\n");
+                case "front" -> sb.append(d.isEmpty() ? -1 : d.peekFirst()).append("\n");
+                case "back" -> sb.append(d.isEmpty() ? -1 : d.peekLast()).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
